5a895dcb2cdfd0bfeaef9665cb4224c775567c96
Tianyi Ma <tn.ma@outlook.com>
2015-08-31 20:05:23 -0500

    Library view renders albums

--- src/app/view.js
+++ src/app/view.js
@@ -101,7 +101,7 @@ $('#full').click(function(e) {
 });
 //Side bar buttons
 $('#albums').click(function(e) {
-  var albumObj = {
+  /*var albumObj = {
     meta: {
       name: lib.albums[playingAlbum].meta.name,
       year: lib.albums[playingAlbum].meta.year.split('-')[0],
@@ -111,7 +111,11 @@ $('#albums').click(function(e) {
     },
     tracks: lib.albums[playingAlbum].tracks
   };
-  loadView('/album-view', albumObj);
+  loadView('/album-view', albumObj);*/
+  var libraryObj = {
+    albums: lib.albums
+  }
+  loadView('/library-view', libraryObj);
 });
 $('#songs').click(function(e) {
   loadView('/playlist-view');
@@ -235,7 +239,7 @@ function hideScreen() {
 function syncPlayBtns() {
   $('[id=list-play]').html('play_arrow');
   $('[id=list-play]').parent().parent().removeClass('active');
-  if (playing) {
+  if (playing && $('.view-header').find('#name').html() == playingAlbum) {
     $('[index=' + playingIndex + ']').html('pause');
     $('[index=' + playingIndex + ']').parent().parent().addClass('active');
   }
@@ -256,4 +260,61 @@ function nextTrack(incrementIndex) {
     }
   }
   return {play: true, url: lib.albums[playingAlbum].tracks[(incrementIndex ? ++playingIndex : (Number(playingIndex) + 1))].url};
+}
+function getAlbumArtist(album) {
+  var trackArtists = [];
+  var artistMap = [];
+  if (album.meta.artist.length != 0) {
+    return album.meta.artist[0];
+  }
+  else {
+    return album.tracks[0] == null ? album.tracks[1].meta.artist[0] : album.tracks[0].meta.artist[0];
+  }
+}
+//https://jsperf.com/encoding-xhr-image-data/51
+function arrayBufferDataUri(raw) {
+  var base64 = ''
+  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  
+  var bytes = new Uint8Array(raw)
+  var byteLength = bytes.byteLength
+  var byteRemainder = byteLength % 3
+  var mainLength = byteLength - byteRemainder
+  
+  var a, b, c, d
+  var chunk
+  
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+  
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+    d = chunk & 63 // 63       = 2^6 - 1
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+  }
+  
+  // Deal with the remaining bytes and padding
+  if (byteRemainder == 1) {
+    chunk = bytes[mainLength]
+  
+    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3) << 4 // 3   = 2^2 - 1
+    base64 += encodings[a] + encodings[b] + '=='
+  } else if (byteRemainder == 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+  
+    a = (chunk & 16128) >> 8 // 16128 = (2^6 - 1) << 8
+    b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15) << 2 // 15    = 2^4 - 1
+    base64 += encodings[a] + encodings[b] + encodings[c] + '='
+  }
+  
+  return 'data:image/jpeg;base64,' + base64
 }
